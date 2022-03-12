aa05c65a30c89b627e410bf79db54f232637d1a0
hel1_yeah <hel1_yeah@ukr.net>
2022-03-12 11:22:10 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  build: {
+    chunkSizeWarningLimit: 1600,
+    assetsInlineLimit: 0,
+    lib: 0,
+  },
 });
 
 // npm run dev
